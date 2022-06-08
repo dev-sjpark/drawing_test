63f75ca37c3c52e9ad6add93d30248425ca20ec3
sjpark <devspec@naver.com>
2022-06-08 18:50:32 +0900

    update: add control panel

--- lib/drawing/drawing_page.dart
+++ lib/drawing/drawing_page.dart
@@ -23,36 +23,51 @@ class DrawingPage extends GetView<DrawingController> {
       child: Scaffold(
         backgroundColor: Colors.white,
         endDrawerEnableOpenDragGesture: false,
-        body: SafeArea(
-          child: Stack(
-            children: [
-              GestureDetector(
-                onScaleStart: controller.onScaleStart,
-                onScaleUpdate: controller.onScaleUpdate,
-                onScaleEnd: controller.onScaleEnd,
-                child: GetBuilder<DrawingController>(
-                  id: _DrawingRefreshId.painter,
-                  builder: (controller) {
-                    return CustomPaint(
-                      foregroundPainter: _DrawingPainter(
-                        lines: controller.lines,
-                      ),
-                      child: Container(color: Colors.transparent,),
-                    );
-                  },
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              child: SafeArea(
+                bottom: false,
+                child: Stack(
+                  children: [
+                    _canvas,
+
+                    _backBtn,
+                  ],
                 ),
               ),
+            ),
 
-              _backBtn(context),
-            ],
-          ),
+            _controlPanel,
+          ],
         ),
       ),
     );
   }
 
+  /// 그림을 그릴 도화지 부분
+  Widget get _canvas => GestureDetector(
+    onScaleStart: controller.onScaleStart,
+    onScaleUpdate: controller.onScaleUpdate,
+    onScaleEnd: controller.onScaleEnd,
+    child: GetBuilder<DrawingController>(
+      id: _DrawingRefreshId.painter,
+      builder: (controller) {
+        return CustomPaint(
+          isComplex: true,
+          willChange: true,
+          foregroundPainter: _DrawingPainter(
+            lines: controller.lines,
+          ),
+          child: Container(color: Colors.transparent,),
+        );
+      },
+    ),
+  );
+
   /// 페이지에 사용되는 뒤로가기 버튼
-  Widget _backBtn(BuildContext context) => Align(
+  Widget get _backBtn => Align(
     alignment: Alignment.topLeft,
     child: IconButton(
       onPressed: () => Get.back(),
@@ -60,6 +75,27 @@ class DrawingPage extends GetView<DrawingController> {
         Icons.arrow_back_ios_new,
         color: Colors.black[50],
       ),
+    ),
+  );
+
+  /// 컨트롤 패널
+  Widget get _controlPanel => Container(
+    decoration: BoxDecoration(
+      borderRadius: const BorderRadius.vertical(top: Radius.circular(16)),
+      color: Colors.white,
+      boxShadow: [BoxShadow(
+        color: Colors.black[20],
+        blurRadius: 5,
+      )]
+    ),
+    padding: EdgeInsets.fromLTRB(
+      16, 12, 16, Get.mediaQuery.viewPadding.bottom + 12,
+    ),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+
+      ],
     ),
   );
 }
